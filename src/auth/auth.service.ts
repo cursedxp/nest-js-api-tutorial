beae975b97016453074c219c3ec4b2c31870316d
Anil Ozsoy <anilozsoy@gmail.com>
2023-07-13 15:35:58 +0200

    updated service file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 
 @Injectable() //Decorator that marks a class as a provider.
 export class AuthService {
   signup() {
-    return { msg: 'Ihave signed up' };
+    return { msg: 'I have signed up' };
   }
   signin() {
-    return { msg: 'Ihave signed in' };
+    return { msg: 'I have signed in' };
   }
 }
